d430ba89ff5e4ad878047122cca46b29875e792d
swvitaliy <sw.vitaliy@gmail.com>
2014-07-15 20:12:52 +0400

    link phpunit container with mysql; update link to mysql container

--- php53-phpunit/run.sh
+++ php53-phpunit/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo docker run -t -i -v /home/vit/local/$1:/opt/$2:rw swvitaliy/php53-phpunit
+CONTAINER_DB_NAME="mysqldb1"
+COMPOSER_GLOBAL_CONTAINER_DIR="/home/worker/.composer"
+COMPOSER_GLOBAL_HOST_DIR=$(readlink -f ./.composer)
+
+docker run -t -i --link $CONTAINER_DB_NAME:$CONTAINER_DB_NAME \
+	-v $COMPOSER_GLOBAL_CONTAINER_DIR:$COMPOSER_GLOBAL_HOST_DIR \
+	-v /home/vit/local/$1:/opt/$2:rw swvitaliy/php53-phpunit

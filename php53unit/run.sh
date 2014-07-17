bdaf3d35068726d38deb15959407d0d3e6993173
swvitaliy <sw.vitaliy@gmail.com>
2014-07-17 18:05:16 +0400

    fix php53unit

--- php53unit/run.sh
+++ php53unit/run.sh
@@ -3,4 +3,4 @@
 CONTAINER_DB_NAME="mysqldb1"
 
 docker run -t -i --link $CONTAINER_DB_NAME:$CONTAINER_DB_NAME \
-	-v /home/vit/local/$1:/opt/$2:rw swvitaliy/php53-phpunit
+	-v /home/vit/local/$1:/opt/$2:rw swvitaliy/php53unit
